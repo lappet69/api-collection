6baf8ced63d5db91c7b095f2fa00edda8f46935d
lappet69 <sinabariba.andre@gmail.com>
2023-05-15 11:55:25 +0700

    update db config

--- src/db/config/database.js
+++ src/db/config/database.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Use this if you encounter "self signed certificate" error
+      },
+    },
   },
   test: {
     username: "postgres",
