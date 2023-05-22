4761ea75c31207da95262b4481bd2ad9ae6a82a9
lappet69 <sinabariba.andre@gmail.com>
2023-05-22 20:50:00 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const PORT = process.env.PORT || 9000;
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+// const PORT = process.env.PORT || 9000;
+// app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
 
 module.exports = app;
