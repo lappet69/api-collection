6dc896e5b13d05a020a90b78442b651ba9f3aee0
lappet69 <sinabariba.andre@gmail.com>
2023-05-22 16:16:33 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
+const PORT = process.env.PORT || 9000;
 const cors = require("cors");
 const router = require("./api/index");
 
