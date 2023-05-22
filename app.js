f9719aabeffe526485e62f66676184857f508022
lappet69 <sinabariba.andre@gmail.com>
2023-05-22 20:20:34 +0700

    update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(
   express.static("public", {
     setHeaders: (res, path) => {
       if (path.endsWith(".css")) {
-        res.setHeader("Content-Type", "text/css");
+        res.setHeader("Content-Type", "text/css", "charset=utf-8");
       }
     },
   })
