4ad97be873edfbd7294cfd82a974f732b099322e
lappet69 <sinabariba.andre@gmail.com>
2023-05-22 20:59:09 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 // const PORT = process.env.PORT || 9000;
 const cors = require("cors");
 const router = require("./src/router/index");
@@ -9,16 +10,11 @@ const swaggerFile = require("./openapi");
 const swaggerUI = require("swagger-ui-express");
 
 // Serve static files
-app.use(
-  express.static("/docs", {
-    setHeaders: (res, path, stat) => {
-      console.log(path.endsWith(".css"));
-      if (path.endsWith(".css")) {
-        res.setHeader("Content-Type", "text/css");
-      }
-    },
-  })
-);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("*.css", (req, res, next) => {
+  res.set("Content-Type", "text/css");
+  next();
+});
 
 app.use(express.json());
 app.use(cors());
