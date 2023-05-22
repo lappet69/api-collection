5a92600277456ee2ff61b0da3e160dec3862c426
lappet69 <sinabariba.andre@gmail.com>
2023-05-22 15:53:20 +0700

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+const router = require("./src/api/index");
+
+const swaggerFile = require("./openapi");
+const swaggerUI = require("swagger-ui-express");
+
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+app.use("/api/v1", router);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
+const app = require("./app");
 const PORT = process.env.PORT || 9000;
-const cors = require("cors");
-const router = require("./src/api/index");
-
-const swaggerFile = require("./openapi");
-const swaggerUI = require("swagger-ui-express");
-
-app.use(express.json());
-app.use(cors());
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
-app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
-});
-app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
-app.use("/api/v1", router);
-
-module.exports = app;
